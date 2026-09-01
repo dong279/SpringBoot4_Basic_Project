6f57e1225831e4eae2ab107f05b3c7e82e69a0e5
dong279 <dongho0130@daum.net>
2026-09-01 16:29:45 +0900

    CustomerRepository add

--- src/main/java/com/rookies6/myspringboot4project/repository/CustomerRepository.java
+++ src/main/java/com/rookies6/myspringboot4project/repository/CustomerRepository.java
@@ -1,4 +1,12 @@
 package com.rookies6.myspringboot4project.repository;
 
-public interface CustomerRepository {
+import com.rookies6.myspringboot4project.Entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    //finder (query) method
+    //1. customerId(고객번호, Unique이므로 중복허용하지않음)로 조회하는 finder 매서드
+    Optional<Customer> findByCustomerName(String customerId);
 }
