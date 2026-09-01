3610770f5cfd27d9e4f56335df874e7a4f8ceeca
dong279 <dongho0130@daum.net>
2026-09-01 10:32:10 +0900

    profile update

--- src/main/java/com/rookies6/myspringboot4project/config/TestConfig.java
+++ src/main/java/com/rookies6/myspringboot4project/config/TestConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-//@Profile("test")
+@Profile("test")
 @Configuration
 public class TestConfig {
     @Bean
