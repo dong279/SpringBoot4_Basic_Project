e1f09e228ca25c19c3b93b08618a6c1817f7cfe8
dong279 <dongho0130@daum.net>
2026-09-01 14:48:32 +0900

    Customer update

--- src/main/java/com/rookies6/myspringboot4project/Entity/Customer.java
+++ src/main/java/com/rookies6/myspringboot4project/Entity/Customer.java
@@ -1,11 +1,19 @@
 package com.rookies6.myspringboot4project.Entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-
 @Table(name = "customers")
+@Getter @Setter
 public class Customer {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String customerId;
+
+    @Column(nullable = false)
+    private String customerName;
 }
