9a9dbc016fc6494208e677c3b7ed61517014c544
dong279 <dongho0130@daum.net>
2026-09-02 15:45:32 +0900

    userRestController update

--- src/main/java/com/rookies6/myspringboot4project/controller/userRestController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/userRestController.java
@@ -1,6 +1,8 @@
 package com.rookies6.myspringboot4project.controller;
 
+import com.rookies6.myspringboot4project.repository.CustomerRepository;
 import com.rookies6.myspringboot4project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class userRestController {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    //Constructor Injection
-    public userRestController(UserRepository userRepository) {
-        log.info("UserRepository 구현 클래스명 = {}",userRepository.getClass().getName());
-        this.userRepository = userRepository;
-    }
+    //Constructor Injection - Mock 객체 주입이 가능
+//    public userRestController(UserRepository userRepository) {
+//        log.info("UserRepository 구현 클래스명 = {}",userRepository.getClass().getName());
+//        this.userRepository = userRepository;
+//    }
 }
