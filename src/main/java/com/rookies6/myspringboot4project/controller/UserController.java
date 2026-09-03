ba6878df74637a17b306f73740a44eebfd286700
dong279 <dongho0130@daum.net>
2026-09-03 16:34:58 +0900

    add-user controller update

--- src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
@@ -48,9 +48,9 @@ public class UserController {
             return "add-user";
         }
         userRepository.save(user);
-        model.addAttribute("users", userRepository.findAll());
-        return "index";
-//        return "redirect:/index";
+//        model.addAttribute("users", userRepository.findAll());
+//        return "index";
+        return "redirect:/index";
     }
 
     @GetMapping("/edit/{id}")
