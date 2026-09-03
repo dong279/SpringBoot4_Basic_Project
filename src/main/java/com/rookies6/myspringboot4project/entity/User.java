c44632d533a69b36aacdc7e4f10ed91233c17443
dong279 <dongho0130@daum.net>
2026-09-03 15:33:00 +0900

    user update

--- src/main/java/com/rookies6/myspringboot4project/entity/User.java
+++ src/main/java/com/rookies6/myspringboot4project/entity/User.java
@@ -1,6 +1,7 @@
 package com.rookies6.myspringboot4project.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -23,6 +24,7 @@ public class User {
 
     @Column(unique = true, nullable = false)
     @NotBlank(message = "email 주소는 필수 입력 항목입니다.")
+    @Email
     private String email;
 
     @Column(nullable = false, updatable = false)
