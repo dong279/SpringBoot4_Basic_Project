a26ef40c29e97a75f7cd8d0ef48c67764656d585
dong279 <dongho0130@daum.net>
2026-09-03 11:00:20 +0900

    WebConfig add

---
+++ src/main/java/com/rookies6/myspringboot4project/config/WebConfig.java
@@ -0,0 +1,9 @@
+package com.rookies6.myspringboot4project.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+}
