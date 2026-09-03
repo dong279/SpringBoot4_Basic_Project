85dd38e348c64b41b8df91fa19da22d8407d2fe9
dong279 <dongho0130@daum.net>
2026-09-03 09:44:07 +0900

    controller getuser add

--- src/main/java/com/rookies6/myspringboot4project/controller/userRestController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/userRestController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -36,4 +37,10 @@ public class userRestController {
                 ()->new BusinessException("User Not Found",HttpStatus.NOT_FOUND));
         return existUser;
     }
+
+    @GetMapping
+    public List<User> getUser(){
+        return userRepository.findAll();
+    }
+
 }
