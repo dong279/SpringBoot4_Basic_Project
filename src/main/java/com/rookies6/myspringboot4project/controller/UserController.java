b4cd4ce36064b0311dc650c85d58805d9c46c672
dong279 <dongho0130@daum.net>
2026-09-03 13:36:19 +0900

    UserController update

--- src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
@@ -1,14 +1,24 @@
 package com.rookies6.myspringboot4project.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserController userController;
+
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
         model.addAttribute("name", "스프링부트");
         return "leaf";
+    }
+
+    @GetMapping("/index")
+    public ModelAndView userlist() {
+
     }
 }
