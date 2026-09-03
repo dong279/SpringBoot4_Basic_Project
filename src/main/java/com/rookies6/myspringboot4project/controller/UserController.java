4e52aee929c4538c9ad9e390e5e9f8a5a8c09c01
dong279 <dongho0130@daum.net>
2026-09-03 13:38:03 +0900

    UserController update

--- src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.rookies6.myspringboot4project.controller;
 
+import com.rookies6.myspringboot4project.entity.User;
+import com.rookies6.myspringboot4project.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class UserController {
-    private final UserController userController;
+    private final UserRepository userRepository;
 
     @GetMapping("/thymeleaf")
     public String leaf(Model model) {
@@ -19,6 +23,7 @@ public class UserController {
 
     @GetMapping("/index")
     public ModelAndView userlist() {
-
+        List<User> userList = userRepository.findAll();
+        return new ModelAndView();
     }
 }
