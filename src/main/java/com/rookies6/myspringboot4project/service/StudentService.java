d75522dd0db974945fcaee5dc4dde86773706812
dong279 <dongho0130@daum.net>
2026-09-08 09:48:41 +0900

    findAll N+1 해결

--- src/main/java/com/rookies6/myspringboot4project/service/StudentService.java
+++ src/main/java/com/rookies6/myspringboot4project/service/StudentService.java
@@ -22,12 +22,18 @@ public class StudentService {
     private final StudentDetailRepository studentDetailRepository;
 
     public List<StudentDTO.Response> getAllStudents() {
-        return studentRepository.findAll()
-                .stream()
-                .map(studentEntity -> StudentDTO.Response.fromEntity(studentEntity))
-//                .map(StudentDTO.Response::fromEntity)
-                .toList();
+        //        return studentRepository.findAll()
+//                .stream()
+//                .map(studentEntity -> StudentDTO.Response.fromEntity(studentEntity) )
+//                //.map(StudentDTO.Response::fromEntity)
+//                .toList();
         //.collect(Collectors.toList());
+
+        //findAll() 대신 Fetch Join 을 사용하여 N+1 문제를 해결한다
+        return studentRepository.findAllWithStudentDetail()
+                .stream()
+                .map(StudentDTO.Response::fromEntity)
+                .toList();
     }
 
     //PK로 학생조회 (FETCH JOIN)
